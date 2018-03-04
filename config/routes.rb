32009a20bc0991f287ebface00f371f61d345268
kat tsukuda <bearheartsbat@gmail.com>
2018-03-03 19:32:08 -0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: "drawings#index"
-
-  get "/drawings", to: "drawings#index", as: "drawings"
-
+  root to: 'drawings#index'
+  get '/about' => 'about#about'
+  get '/contact' => 'contact#index'
+  get '/drawings' => 'drawings#index'
+  get '/projects' => 'projects#index'
 end
